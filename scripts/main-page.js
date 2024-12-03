2ff93dbd3365e886e4dd92067ab20182d6e15276
Nazim Dovlatov <159007876+0MrStyx0@users.noreply.github.com.>
2024-12-03 20:49:37 +0400

    Delete mechanism created. Check empty list created  Changes to be committed: 	modified:   pages/main/task-list.css 	modified:   scripts/main-page.js

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -2,8 +2,11 @@ const form = document.querySelector('form.add-task');
 const taskNameInput = form.querySelector('input.task-name');
 const taskDescriptionInput = form.querySelector('input.task-description');
 const tasksList = document.querySelector('ol.tasks-list');
+const emptyList = document.querySelector('div.empty-list');
 
 form.addEventListener('submit', addTask);
+
+tasksList.addEventListener('click', deleteTask);
 
 function addTask(event) {
     event.preventDefault();
@@ -12,9 +15,9 @@ function addTask(event) {
     const taskDescription = taskDescriptionInput.value;
 
     const taskHtml = `<li>${taskName}
-                    <input type="checkbox">
+                    <input type="checkbox" data-action="done">
                     <button class="edit">Edit</button>
-                    <button class="delete">Delete</button>
+                    <button class="delete" data-action="delete">Delete</button>
                 </li>`;
 
 
@@ -22,4 +25,19 @@ function addTask(event) {
 
     taskNameInput.value = "";
     taskDescriptionInput.value = "";
+
+    if(tasksList.children.length > 0) {
+        emptyList.style.display = 'none';
+    }
+}
+
+function deleteTask(event) {
+    if(event.target.dataset.action === 'delete') {
+        const task = event.target.closest('li');
+        task.remove();
+
+        if(tasksList.children.length === 0) {
+            emptyList.style.display = 'block';
+        }
+    }
 }
