a8113c6e0d12655e9de2e725e275dbfd23b7c41d
Nazim Dovlatov <159007876+0MrStyx0@users.noreply.github.com.>
2024-12-03 19:22:17 +0400

    Class Task and TaskList created  Changes to be committed: 	new file:   scripts/Task.js 	new file:   scripts/main-page.js

---
+++ scripts/Task.js
@@ -0,0 +1,32 @@
+class Task {
+    static #id = 0;
+    #name;
+    #description;
+    #date;
+    #status = false;
+
+    constructor(name, description){
+        Task.#id++;
+        this.#name = name;
+        this.#description = description;
+        this.#date = this.#setDate();
+    }
+
+    #setDate() {
+        const now = new Date();
+  
+        const day = String(now.getDate()).padStart(2, '0');
+        const month = String(now.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+        const year = now.getFullYear();
+        
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+      
+        return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+    }
+}
+
+class TaskList {
+    #list;
+}
